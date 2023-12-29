a66e9cda94cd892b068286c041bb91088c3a6351
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-12-29 11:44:18 +0530

    t-12

--- redux-pr.js
+++ redux-pr.js
@@ -1,8 +1,15 @@
 const redux=require('redux');
 
 const counterReducer =(state={counter:0},action)=>{
-return {
-    counter:state.counter-1
+// return {
+//     counter:state.counter-1
+// }
+if (action.type === 'INCREMENT') {
+    return { counter: state.counter + 2 };
+} else if (action.type === 'DECREMENT') {
+    return { counter: state.counter - 2 };
+} else {
+    return state;
 }
 }
 const store=redux.createStore(counterReducer);
@@ -13,7 +20,10 @@ const counterSubscriber=()=>{
 }
 
 store.subscribe(counterSubscriber)
-store.dispatch({type: 'decrement'})
+// store.dispatch({type: 'decrement'})
 // store.dispatch({type: 'increment'})
 // store.dispatch({type: 'increment'})
 // store.dispatch({type: 'increment'})
+store.dispatch({ type: 'INCREMENT' }); 
+store.dispatch({ type: 'DECREMENT' });
+
